455f0d59de04750315bb0242b63f58c0ae0304e7
Hery <hery@serasera.org>
2023-04-26 13:11:02 +0200

    Add google auth

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,26 +1,27 @@
 <template>
     <h1>Register</h1>
     <div class="mb-3">
-      <label for="email" class="form-label">Email</label>
-      <input type="email"
-        class="form-control" name="email" id="email" aria-describedby="emailhelp" placeholder="Votre email" v-model="email">
-      <small id="emailhelp" class="form-text text-muted">Insérer l'email</small>
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailhelp"
+            placeholder="Votre email" v-model="email">
+        <small id="emailhelp" class="form-text text-muted">Insérer l'email</small>
     </div>
     <div class="mb-3">
-      <label for="password" class="form-label">Password</label>
-      <input type="password"
-        class="form-control" name="password" id="password" aria-describedby="passHelp" placeholder="" v-model="password">
-      <small id="passHelp" class="form-text text-muted">Your password</small>
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" name="password" id="password" aria-describedby="passHelp" placeholder=""
+            v-model="password">
+        <small id="passHelp" class="form-text text-muted">Your password</small>
     </div>
-    <button type="button" @click="register" class="btn btn-primary">Register</button>
+    <button type="button" @click="register" class="btn btn-primary me-3">Register</button>
+    <button type="button" @click="signInWithGoogle" class="btn btn-primary">Sign in with Google</button>
 </template>
 
 <script setup>
-import  {ref} from "vue"
-import { getAuth, createUserWithEmailAndPassword} from "firebase/auth"
+import { ref } from "vue"
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 import { useRouter } from "vue-router";
 
-const email= ref("")
+const email = ref("")
 const password = ref("")
 
 const router = useRouter()
@@ -33,7 +34,19 @@ const register = () => {
         console.log(error.code);
         alert(error.message);
     })
-    
+
 }
 
+const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(getAuth(), provider)
+    .then((result) => {
+        console.log(result.user);
+        router.push('/feed');
+    })
+    .catch((error) => {
+        console.log(error.code);
+        alert(error.message);
+    })
+}
 </script>
